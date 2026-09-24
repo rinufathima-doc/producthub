4608d0e013b310b3f057b52c709b59e14b39f441
rinufathima-doc <rinufathimadoc@gmail.com>
2026-09-24 11:08:11 +0530

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import ProductDetails from "./pages/ProductDetails";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import Seller from "./pages/Seller";
+import Seller  from "./pages/Seller.jsx";
 
 function App() {
   const [products, setProducts] = useState([]);
